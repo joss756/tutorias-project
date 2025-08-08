64fea8daf6d4259e721e8284ed8080c08d44aba1
joss756 <joseperezromero183@gmail.com>
2025-08-07 19:21:39 -0600

    Add files via upload

---
+++ dist/project-tutorias/browser/chunk-JAF25VWV.js
@@ -0,0 +1,1 @@
+import{Ia as n,U as i,Ua as m,x as t}from"./chunk-PBJI7WEI.js";var f=[],e=class o{static \u0275fac=function(r){return new(r||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[m.forChild(f),m]})};var s=class o{static \u0275fac=function(r){return new(r||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[n,e]})};export{s as SeguimientosModule};
---
+++ dist/project-tutorias/browser/chunk-PUFMYDJG.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-KCE2TZJS.js";import"./chunk-7FHZTGIO.js";import"./chunk-PBJI7WEI.js";export{a as MateriasModule};
