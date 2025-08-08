6d3b17c088236d9d21420dbc784177be14123887
joss756 <joseperezromero183@gmail.com>
2025-08-07 20:00:31 -0600

    Create prender.ts

---
+++ src/prender.ts
@@ -0,0 +1,5 @@
+export const prerenderRoutes = [
+  '/dashboard/materias/editar/1',  // Ejemplo de ruta con parámetro
+  '/dashboard/materias/editar/2',  // Otro ejemplo de ruta con parámetro
+  // Agrega más rutas prerenderizadas aquí si es necesario
+];
