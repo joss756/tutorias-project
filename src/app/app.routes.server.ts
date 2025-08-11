89c3025d8f9922b77baa338f64cf0ebd5ab24da5
joss756 <joseperezromero183@gmail.com>
2025-08-11 12:43:53 -0600

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Ruta dinámica que debe renderizarse solo en el servidor
+  {
+    path: 'dashboard/materias/editar/:id',
+    renderMode: RenderMode.Server
+  },
+  // El resto de rutas se pueden prerenderizar
   {
     path: '**',
     renderMode: RenderMode.Prerender
